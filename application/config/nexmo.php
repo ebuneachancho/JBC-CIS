10e9a8d2c7fd2dc5ae6575dcb5ec936c96e0df83
Ebune Joseph A <ebuneachancho@gmail.com>
2018-02-11 18:57:47 +0100

    Add files via upload

---
+++ application/config/nexmo.php
@@ -0,0 +1,8 @@
+<?php  (! defined('BASEPATH')) and exit('No direct script access allowed');
+
+// please get the following api on Nexmo Dashboard.
+$config['api_key'] = 'd46da17c';
+$config['api_secret'] = 'ecda7d5277ca97d7';
+
+/* End of file nexmo.php */
+/* Location: ./application/config/nexmo.php */
